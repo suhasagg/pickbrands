d0df491bc89b9a133bf78fc75c30d40903719637
suhasagg <suhasagg@gmail.com>
2021-07-12 03:28:31 +0530

    Pick brands game

---
+++ pickbrands/header.php
@@ -0,0 +1,27 @@
+<div id="header">
+			<div id="title">
+				<div id="left">
+					<center class="dg"></center>
+
+					<center class="lb">W</center>
+					<center class="dg">W</center>
+					<center class="lg">E</center>
+                    <center class="lb"></center>
+					<center class="dg"></center>
+
+
+				</div>
+				<div id="right">
+                    <center class="lo"> </center>
+					<center class="lp">M</center>
+					<center class="lr">O</center>
+					<center class="ly">M</center>
+					<center class="lp">E</center>
+					<center class="lr">N</center>
+					<center class="ly">T</center>
+                    <center class="lp">S</center>
+					<center class="lr"> </center>
+
+				</div>
+			</div>
+</div>
